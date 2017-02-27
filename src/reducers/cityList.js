4c11cfc253aa84b835080c989126bf547ef87ac2
Maksim <maksim.shkarbaliuk@sotsos.com>
2017-02-27 14:55:55 +0300

    createStore - made store, add default reducers

---
+++ src/reducers/cityList.js
@@ -0,0 +1,10 @@
+const initialState = {
+
+
+}
+
+export default cityList = (state=initialState, action)=>{
+    return {
+        ...state
+    }
+}
---
+++ src/reducers/forecast.js
@@ -0,0 +1,10 @@
+const initialState = {
+
+
+}
+
+export default forecast = (state=initialState, action)=>{
+    return {
+        ...state
+    }
+}
